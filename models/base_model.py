057495e5db3007e9698b2876e2101978bc7ffdc3
TeddyMuli <tmuli974@gmail.com>
2023-05-12 14:31:07 +0300

    models

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Class that defines all common attributes/methods for other classes"""
+
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """A class that defines all common attributes/methods for other classes"""
+    def __init__(self, *args, **kwargs):
+        """The constructor"""
+
+        #from models import storage
+        if not kwargs:
+            self.id = str(uuid4)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        
+    def __str__(self):
+        """Retirns the string representation of the object"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+    
+    #public instance methods
+    def save(self):
+        """Updates the public instance attribute updated at"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance:"""
+        object = self.__dict__.copy()
+        object['__class__'] = self.__class__.__name__
+        object['created_at'] = self.created_at.isoformat()
+        object['updated_at'] = self.updated_at.isoformat()
+        return object
